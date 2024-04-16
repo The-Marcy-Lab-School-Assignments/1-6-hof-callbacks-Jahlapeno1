1f0cb7878138643c584ffb24d3cfaef5b4f7d4d2
Jahlapeno1 <jimenezaot@gmail.com>
2024-04-16 18:31:23 -0400

    completed from-scratch

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,28 +1,66 @@
-const myForEach = () => {
+const myForEach = (arr, callbFunc) => {
+  for (const name of arr) {
+    callbFunc(name)
+  }
 };
 
-const myMap = () => {
+const myMap = (arr, map) => {
+  const newArr = []
+  for (const num of arr) {
+    newArr.push(map(num))
+  }
+  return newArr
 };
 
-const myFind = () => {
+const myFind = (arr, find) => {
+  for (const index of arr) {
+    if (find(index)) {
+      return index
+    }
+  }
 };
 
-const myFilter = () => {
+const myFilter = (arr, filter) => {
+  newArr = []
+  for (const value of arr) {
+    if (filter(value)) {
+      newArr.push(value)
+    }
+  } return newArr
 };
 
-const sortWords = () => {
+const sortWords = (arr) => {
+  const sortedArr = [...arr]
+  // console.log(sortedArr.sort())
+  return sortedArr.sort()
 };
 
-const sortNumbers = () => {
+const sortNumbers = (arr) => {
+  const sortedArr = [...arr]
+  return sortedArr.sort((a, b) => a - b)
 };
 
-const sortNumbersBetter = () => {
+const sortNumbersBetter = (arr, boolean) => {
+  const sortedArr = [...arr]
+  return boolean ? sortedArr.sort((a, b) => b - a) : sortedArr.sort((a, b) => a - b)
 };
 
-const sortUsersByOrder = () => {
+const sortUsersByOrder = (arrOfObj) => {
+  const sortedArr = [...arrOfObj]
+  return sortedArr.sort((a, b) => a.order - b.order)
 };
 
-const sortUsersByName = () => {
+const sortUsersByName = (arrOfObj) => {
+  const sortedArr = [...arrOfObj]
+  return sortedArr.sort((a, b) => {
+    if (a.name < b.name) {
+      return -1
+    } else if (a.name > b.name) {
+      return 1
+    } else {
+      return 0
+    }
+  })
 };
 
 module.exports = {
